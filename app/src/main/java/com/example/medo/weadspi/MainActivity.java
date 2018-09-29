37d5b376a66b0da07c3067e0bbe518f9494cb0a4
Mohammed Shalabi <idevmedo@gmail.com>
2018-09-29 13:28:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/medo/weadspi/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.medo.weadspi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity {
+
+    Button button1 ;
+    TextView textView1 ;
+    TextView textView;
+    WebView webView;
+    int count;
+    String uidValue;
+
+    VideoView videoView;
+    WebView mWebView;
+
+    private final String YOUTUBE_PREFIX = "https://www.youtube.com/embed/";
+    private void setupWebView(){
+        webView.getSettings().setJavaScriptEnabled(true);
+        String code = "A0K5KFKKhYY";
+        webView.loadUrl(YOUTUBE_PREFIX+code);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // button1 = (Button) findViewById(R.id.button);
+        textView1 = (TextView) findViewById(R.id.textView2);
+        textView = (TextView) findViewById(R.id.textView);
+        webView = findViewById(R.id.webview);
+        count = 0 ;
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef1 = database.getReference("Screens/Screen1/UID");
+        final DatabaseReference myRef2 = database.getReference("users");
+
+        setupWebView();
+
+        myRef1.addValueEventListener(new ValueEventListener(){
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                uidValue = String.valueOf(dataSnapshot.getValue());
+
+
+                String value = String.valueOf(dataSnapshot.getValue());
+                System.out.println("THIS IS MY INTEREST 7 :"+value);
+                textView1.setText(value);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // Read from the database
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    User user = userSnapshot.getValue(User.class);
+                    // if(user.getId().equals(uidValue)){
+                    textView.setText("ID : "+user.getId()+" NAME : "+user.getName()+" INT :"+user.getIntreset());
+                    // }
+                }
+
+               // videoView.setVideoPath();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/medo/weadspi/Screen.java
@@ -0,0 +1,10 @@
+package com.example.medo.weadspi;
+
+public class Screen {
+    private String SID;
+    private String UID;
+
+    public String getSID() {
+        return SID;
+    }
+}
