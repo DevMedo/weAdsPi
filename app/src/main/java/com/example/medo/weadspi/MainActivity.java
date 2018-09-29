d1bbeb86c5537c951342e9b9e51919a60c6da1df
Mohammed Shalabi <idevmedo@gmail.com>
2018-09-29 16:00:08 +0300

    Changes lots of things

--- app/src/main/java/com/example/medo/weadspi/MainActivity.java
+++ app/src/main/java/com/example/medo/weadspi/MainActivity.java
@@ -35,15 +35,18 @@ import static android.content.ContentValues.TAG;
 public class MainActivity extends Activity {
 
     Button button1 ;
-    TextView textView1 ;
-    TextView textView;
+    //TextView textView1 ;
+    //TextView textView;
     WebView webView;
     int count;
     String uidValue;
     String inte;
-
+    String Hackthon="hackathon";
+    String carString="cars";
+    String sports="sports";
     VideoView videoView;
     WebView mWebView;
+    String TEMP;
 
     //YouTubePlayerView pw;
 
@@ -60,12 +63,16 @@ public class MainActivity extends Activity {
         webView.loadUrl(YOUTUBE_PREFIX+code);
     }
 
+
     private void sports(){
         webView.getSettings().setJavaScriptEnabled(true);
-        String code = "MEoDYm0v4j0";
+              String code = "MEoDYm0v4j0";
         webView.loadUrl(YOUTUBE_PREFIX+code);
     }
 
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef1 = database.getReference("Screens/Screen1/UID");
+    final DatabaseReference myRef2 = database.getReference("users");
 
 
     @Override
@@ -73,15 +80,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // button1 = (Button) findViewById(R.id.button);
-        textView1 = (TextView) findViewById(R.id.textView2);
-        textView = (TextView) findViewById(R.id.textView);
+//        textView1 = (TextView) findViewById(R.id.textView2);
+//        textView = (TextView) findViewById(R.id.textView);
         webView = findViewById(R.id.webview);
         count = 0 ;
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef1 = database.getReference("Screens/Screen1/UID");
-        final DatabaseReference myRef2 = database.getReference("users");
+
+
+
 
         //setupWebView();
 
@@ -90,13 +96,17 @@ public class MainActivity extends Activity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
+                TEMP = uidValue;
                 uidValue = String.valueOf(dataSnapshot.getValue());
 
 
-                String value = String.valueOf(dataSnapshot.getValue());
-                System.out.println("THIS IS MY INTEREST 7 :"+value);
-                textView1.setText(value);
+            //    String value = String.valueOf(dataSnapshot.getValue());
+              //  System.out.println("THIS IS MY INTEREST 7 :"+value);
+//                textView1.setText(value);
 
+                if (TEMP != uidValue){
+                    CheckUserProfile();
+                }
 
             }
 
@@ -107,44 +117,74 @@ public class MainActivity extends Activity {
             }
         });
         // Read from the database
+
+    }
+    public void CheckUserProfile(){
         myRef2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
                     User user = userSnapshot.getValue(User.class);
-                     if(user.getId().equals(uidValue)){
-                         // Just for testing
-                    textView.setText("ID : "+user.getId()+" NAME : "+user.getName()+" INT :"+user.getIntreset());
-                    inte=user.getIntreset();
-                         Log.d("HERE IT IS",inte+" $ "+user.getIntreset());
-                     }
-                }
+                    if(user.getId().equals(uidValue)){
+                        // Just for testing
+                        //textView.setText("ID : "+user.getId()+" NAME : "+user.getName()+" INT :"+user.getIntreset());
 
 
-                String Hackthon="hackathon";
-                String carString="cars";
-                String sports="sports";
+                        inte=user.getIntreset();
+                        Log.d("HERE IT IS",inte+" $ "+user.getIntreset());
+                    }
 
-                    //hackathon();
-                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
-                    User user = userSnapshot.getValue(User.class);
+                    /////////////////////////
+                    //user = userSnapshot.getValue(User.class);
                     //System.out.println(" inte "+inte);
                     System.out.println(Hackthon+" # "+inte);
                     if(Hackthon.equals(inte)){
                         // Just for testing
+
                         hackathon();
+                        inte="";
+
                     }else if(carString.equals(inte)){
+
                         cars();
+                        inte="";
+
 
                     }
+                    else if(sports.equals(inte)){
+
+                        sports();
+                        inte="";
+
+
+                    }
+
+                    //////////////////////
                 }
 
 
 
 
+//                    //hackathon();
+//                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+//                    User user = userSnapshot.getValue(User.class);
+//                    //System.out.println(" inte "+inte);
+//                    System.out.println(Hackthon+" # "+inte);
+//                    if(Hackthon.equals(inte)){
+//                        // Just for testing
+//                        hackathon();
+//                    }else if(carString.equals(inte)){
+//                        cars();
+//
+//                    }
+//                }
 
-               // videoView.setVideoPath();
+
+
+
+
+                // videoView.setVideoPath();
 
             }
 
